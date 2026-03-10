33a8aa983bbd5e993e312e740ce643dd2f454f61
devprashant17 <prashantkarki0617@gmail.com>
2026-03-10 16:44:36 +0545

    Setup done.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import bg from './assets/bg.jpeg'
 import React from "react";
 
 function App(){
-  return <div   style={{ backgroundImage: `url(${bg})` }} className="h-screen bg-cover bg-center">
+  return <div style={{ backgroundImage: `url(${bg})` }} className="h-screen bg-cover bg-center">
     HIii
   </div>
 }
