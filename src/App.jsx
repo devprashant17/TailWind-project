42c0b2e3e2a300e6e3df04226aa78956647b2b4a
devprashant17 <prashantkarki0617@gmail.com>
2026-03-10 16:41:32 +0545

    Created setup for portfolio assignment.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import bg from './assets/bg.jpeg'
+import React from "react";
+
+function App(){
+  return <div   style={{ backgroundImage: `url(${bg})` }} className="h-screen bg-cover bg-center">
+    HIii
+  </div>
+}
+
+export default App;
